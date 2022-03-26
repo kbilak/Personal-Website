e739181bc8af357bd17cd1a0ce50a5a97ee18de2
Krzysztof Białk <krzys.bialk1@gmail.com>
2022-03-26 09:26:56 +0100

    updated passport

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,19 @@
-import passport from "passport";
-import passportLocal from "passport-local";
-
+import { Request, Response, NextFunction } from "express";
 import { User, UserDocument } from "../models/User";
+import passportLocal from "passport-local";
+import { NativeError } from "mongoose"
+import passport from "passport";
 
 const LocalStrategy = passportLocal.Strategy;
+
+passport.serializeUser<any, any>((req, user, done) => {
+    done(undefined, user);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err: NativeError, user: UserDocument) => done(err, user));
+});
+
+const verifyCallback = (email, password, done) => {
+    User.findOne({email: email})
+} 
