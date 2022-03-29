e24e168bf8cd9750f8316cf9e09d9ca41549ff17
Krzysztof Białk <krzys.bialk1@gmail.com>
2022-03-29 21:46:41 +0200

    add jest

--- src/app.ts
+++ src/app.ts
@@ -94,3 +94,5 @@ app.listen(app.get("port"), () => {
     console.log(`App is running at http:localhost:${process.env.PORT || 3000}`);
     console.log("CTRL+C to stop the server");
 });
+
+export default app;
