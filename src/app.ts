3833bd28cd1f8cf36b82a67c535fd5dda735d86c
Krzysztof Białk <krzys.bialk1@gmail.com>
2022-03-30 22:16:47 +0200

    add basic views

--- src/app.ts
+++ src/app.ts
@@ -32,9 +32,9 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
+app.use(express.static(path.join(__dirname, "public"), { maxAge: 31557600000 }));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(__dirname + "/public"));
 app.use(cookieParser());
 app.use(session({ 
     secret: String(process.env.SESSION_SECRET),
